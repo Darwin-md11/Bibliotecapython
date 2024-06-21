5f54454f7afd41a54a478abe08193a854eede338
Darwin-md11 <merchandarwin2@gmail.com>
2024-06-21 12:38:09 -0500

    Delete BIBLIOTECA directory

--- BIBLIOTECA/Categoria.py
+++
@@ -1,6 +0,0 @@
-class Categoria:
-    def __init__(self, nombre):
-        self.nombre = nombre
-
-    def mostrar_info(self):
-        print(f"Categoría: {self.nombre}")
--- BIBLIOTECA/Usuario.py
+++
@@ -1,8 +0,0 @@
-class Usuario:
-    def __init__(self, nombre, apellido, id_usuario):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.id_usuario = id_usuario
-
-    def mostrar_info(self):
-        print(f"Usuario: {self.nombre} {self.apellido} (ID: {self.id_usuario})")
